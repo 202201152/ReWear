57d3f26e4f0233c66d45681aad97247fefc5583c
kanu1101 <202201261@daiict.ac.in>
2025-07-12 11:42:19 +0530

    added user and prodcut models

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,16 @@
 import express from "express"
+import { connectDB } from "./lib/db";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js"
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/auth', authRoutes);
+
+app.listen(process.env.PORT , () => {
+    console.log("server is running.");
+    connectDB();
+
+})
