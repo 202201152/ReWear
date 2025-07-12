f78c70a19ff59ab768d3cbf3d57226ea8afd4976
kanu1101 <202201261@daiict.ac.in>
2025-07-12 16:29:35 +0530

    admin changes

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,13 +1,21 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-export const generateToken = (userId, res) => {
-    const token = jwt.sign({userId}, process.env.JWT_SECRET, {
-        expiresIn: "7d",
-    });
-    res.cookie("jwt", token, {
-        maxAge: 7*24*60*60*1000,
-        sameSite: "lax",
-        httpOnly: true,
-        secure: false,
-    })
-}
+export const generateToken = (user, res) => {
+  const token = jwt.sign(
+    {
+      userId: user._id,
+      role: user.role, // Include role here
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "7d",
+    }
+  );
+
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: false, // Set to true if using HTTPS
+    sameSite: "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
+};
